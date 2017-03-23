e58ae11cfa79bd9d88ff371268bb6b7fecfab0ad
Mike Baker <bakermg@me.com>
2017-03-23 11:07:27 -0400

    Added: Blog post now coming through the api no more a link syled the content and added a close button

--- js/freelancer.js
+++ js/freelancer.js
@@ -41,4 +41,55 @@
         });
     });
 
+    // API call to WordPress Blog
+    $('#blog-button').on('click', function(e) {
+        e.preventDefault();
+        getBlog();
+    });
+
+    //Fire off api call
+    var getBlog = function() {
+        $('#blog-button').hide();
+        var fullContent, contentSlug, excerptContent;
+        $('#content').empty();
+        $.ajax({
+            method: 'GET',
+            url: 'http://blogbybaker.com/wp-json/wp/v2/posts'
+        })
+        .done(function(data) {
+            $('#content').append('<h1>Recent Post</h1');
+             fullContent = '<p>' + data[0].content.rendered + '</p>';
+             contentSlug = '<h2>' + data[0].title.rendered + '</h2>';
+             excerptContent = '<p>' + data[0].excerpt.rendered + '</p>';
+            console.log(contentSlug);
+            $('#content').append(contentSlug, excerptContent);
+
+            $('.more-link').click(function(e) {
+                e.preventDefault();
+                updateContent();
+            });
+        })
+        .fail(function() {
+            $('#content').append('<h1 id="error">Sorry something went wrong try again</h1>');
+        })
+
+        // Update to full content if read more
+        var updateContent = function() {
+            $('#content').empty().append('<h1>Recent Post</h1>', contentSlug, fullContent);
+            $('span').css('color', '#ECEFF1');
+            $('pre span').css('color', '#333');
+            $('#content').append('<a id="close-blog" class="btn btn-lg btn-outline" href="#about"><i class="fa fa-email"></i>Close Blog</a>')
+            $('#close-blog').on('click', function() {
+                $('#content').empty();
+                $('#blog-button').show();
+         })
+        }
+
+    };
+
+
+
+
+
+
 })(jQuery); // End of use strict
