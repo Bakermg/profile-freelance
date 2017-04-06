f7ba0e757e8a49aee67a250886ede5a7a4cb6cfd
Mike Baker <mike@bakerwebco.com>
2017-04-06 19:14:44 -0400

    Fixed app.js for the ajax call to wordpress and also fixed issue with the ajax call not working in firefox because the async was not loading jquery before the ajax was being called in the document get ready

--- js/app.js
+++ js/app.js
@@ -44,12 +44,12 @@
     // API call to WordPress Blog
 
     $(document).ready(function() {
-        // $('#blog-button').on('click', function() {
-            // event.preventDefault();
-            // getBlog();
-        // });
+        $('#blog-button').on('click', function(event) {
+            event.preventDefault();
+            getBlog();
+        });
         var fullContent, contentSlug, excerptContent;
-    // var getBlog = function() {
+  
         $.ajax({
                 method: 'GET',
                 url: 'https://blogbybaker.com/wp-json/wp/v2/posts',
@@ -59,30 +59,24 @@
                 fullContent = '<p>' + data[0].content.rendered + '</p>';
                 contentSlug = '<h2>' + data[0].title.rendered + '</h2>';
                 excerptContent = '<p>' + data[0].excerpt.rendered + '</p>';
-                $('#content').empty();
-                $('#content').append('<h1>Recent Post</h1');
-                $('#content').append(contentSlug, excerptContent);
-
-                $('.more-link').click(function(event) {
-                    event.preventDefault();
-                    updateContent();
-                });
+                
             })
             .fail(function() {
                 $('#content').append('<h3>Sorry something went wrong please reload page</h3>');
             })
 
+        var getBlog = function() {
         //Fire off api call
 
-            // $('#blog-button').hide();
-            // $('#content').empty();
-            // $('#content').append('<h1>Recent Post</h1');
-            // $('#content').append(contentSlug, excerptContent);
+            $('#blog-button').hide();
+            $('#content').empty();
+            $('#content').append('<h1>Recent Post</h1');
+            $('#content').append(contentSlug, excerptContent);
 
-            // $('.more-link').click(function(event) {
-            //     event.preventDefault();
-            //     updateContent();
-            // });
+            $('.more-link').click(function(event) {
+                event.preventDefault();
+                updateContent();
+            });
 
             // Update to full content if read more
             var updateContent = function() {
@@ -92,10 +86,10 @@
                 $('#content').append('<a id="close-blog" class="btn btn-lg btn-outline" href="#about"><i class="fa fa-email"></i>Close Blog</a>')
                 $('#close-blog').on('click', function() {
                     $('#content').empty();
-                    $('#content').append(contentSlug, excerptContent);
-                    // $('#blog-button').show();
+                    $('#blog-button').show();
+        
                 })
             }
-        // }
+         }
     });
 })(jQuery); // End of use strict
